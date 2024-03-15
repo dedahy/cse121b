897054dc8ec7f33010cdaddb7d36be99b6cb6ba8
dedahy <dada.dedahy@gmail.com>
2024-03-15 11:42:00 +0300

    update

--- week2/scripts/w02-task.js
+++ week2/scripts/w02-task.js
@@ -1,6 +1,12 @@
+/* Step 2 - Variables */
+
 let fullName = "Dedahy Jean OMBILAHY";
 let currentYear;
 let profilePicture = "images/myself.jpg";
+let x = document.querySelector("img");
+
+
+const imageElement = document.querySelector('img');
 
 const nameElement = document.getElementById('name');
 
@@ -8,11 +14,7 @@ const foodElement = document.getElementById('food');
 
 const yearElement = document.querySelector('#year');
 
-let imageElement = document.querySelector('img');
-
-let x = document.getElementById("img");
-
-let food = ["Rice", "Corns", "Hamburger", "Pasta"];
+/* W02-Task - Profile Home Page */
 
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 
@@ -20,9 +22,16 @@ imageElement.setAttribute('src', profilePicture);
 
 imageElement.setAttribute('alt', "Profile image of [x]");
 
+/* Step 5 - Array */
+
+let food = ["Rice", "Corns", "Hamburger", "Pasta"];
+
+let nfood = ["Potato with meat"]
 foodElement.innerHTML += `<br>${food}`;
 
-food.push("Fishes", "Cassava", "Chicken")
+/* Step 4 - Adding Content */
+
+food.push(nfood);
 
 foodElement.innerHTML += `<br>${food}`;
 
@@ -31,36 +40,7 @@ foodElement.innerHTML += `<br>${food}`;
 
 food.pop();
 foodElement.innerHTML += `<br>${food}<br><br><br>`;
-
-
-
-
-/* W02-Task - Profile Home Page */
-
+//
 /* Step 1 - Setup type tasks - no code required */
 
-/* Step 2 - Variables */
-
-
-
-
-/* Step 3 - Element Variables */
-
-
-
-
-
-/* Step 4 - Adding Content */
-
-
-
-
-
-
-/* Step 5 - Array */
-
-
-
-
-
-
+//
