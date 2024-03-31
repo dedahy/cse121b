705f5505c06deee6c4f3ce3f08c5493d0dff574a
dedahy <dada.dedahy@gmail.com>
2024-03-31 07:41:27 +0300

    update

---
+++ week3/w03-task.js
@@ -0,0 +1,47 @@
+function add (number1, number2) 
+{
+    return number1 + number2;
+}
+
+function addNumbers(addNumber1, addNumber2)
+{
+  let addNumber1 = Number(document.querySelector('#add1').value);
+  let addNumber2 = Number(document.querySelector('#add2').value);
+
+  return addNumber1 + addNumber2;
+}
+
+document.querySelector('#sum').value = add(addNumber1, addNumber2);
+
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+
+
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
